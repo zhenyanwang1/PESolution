ec44d2314e3e5ec2e5d283c78876ba84a48b2dfc
Zhenyan <zhenyanwang1@gmail.com>
2014-08-21 11:54:20 +0800

    #2

---
+++ 2.py
@@ -0,0 +1,14 @@
+from math import sqrt
+def F(n):
+    return int(((1 + sqrt(5)) ** n - (1 - sqrt(5)) ** n) / (2 ** n * sqrt(5)))
+val=0
+pos=1
+while True:
+    num=F(pos)
+    if num<=4000000:
+        if num%2==0:
+            val+=num
+    else:
+        break
+    pos+=1
+print(val)
