21d9feb7db7b9cc1ee84a2247f34a37ca28f10c2
Zhenyan <zhenyanwang1@gmail.com>
2014-08-30 12:20:36 +0800

    Formatting.

--- 16.py
+++ 16.py
@@ -1,4 +1,5 @@
 import gmpy2
-num=gmpy2.mpz("2")
-num**=1000
+
+num = gmpy2.mpz("2")
+num **= 1000
 print(sum([int(chr) for chr in str(num)]))
--- 17.py
+++ 17.py
@@ -1,7 +1,7 @@
 import string
 
 d1 = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
-d2 = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen","nineteen", "twenty"]
+d2 = ["eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"]
 d3 = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
 
@@ -21,25 +21,26 @@ def convert(n):
         elif n % 10 == 0:
             return d1[n // 100 - 1] + " hundred and " + d3[n % 100 // 10 - 1]
         else:
-            if 1<=n % 100<=10:
-                return d1[n // 100 - 1] + " hundred and " + d1[n % 100 -1]
+            if 1 <= n % 100 <= 10:
+                return d1[n // 100 - 1] + " hundred and " + d1[n % 100 - 1]
             elif 11 <= n % 100 <= 20:
-                return d1[n // 100 - 1] + " hundred and " + d2[n%100 - 11]
+                return d1[n // 100 - 1] + " hundred and " + d2[n % 100 - 11]
             elif 21 <= n % 100 < 100:
                 if n % 100 % 10 != 0:
                     return d1[n // 100 - 1] + " hundred and " + d3[n % 100 // 10 - 1] + "-" + d1[n % 100 % 10 - 1]
                 else:
                     return d1[n // 100 - 1] + " hundred and " + d3[n % 100 // 10 - 1]
-    elif n==1000:
+    elif n == 1000:
         return "one thousand"
 
-snums=[]
-for i in range(1,1000+1):
+
+snums = []
+for i in range(1, 1000 + 1):
     snums.append(convert(i))
 print(snums)
-total=0
+total = 0
 for snum in snums:
     for chr in snum:
         if chr in string.ascii_lowercase:
-            total+=1
+            total += 1
 print(total)
