6477bbcc5da16d4f91a6c52cc7f6ea881d69f4f3
Zhenyan <zhenyanwang1@gmail.com>
2014-08-30 14:34:41 +0800

    Formatting

--- 23.py
+++ 23.py
@@ -1,23 +1,25 @@
 import sympy
 
+
 def d(n):
     return sum(sympy.divisors(n)[:-1])
 
-isabnum=[]
-for num in range(1,28123 + 1):
-    isabnum.append(d(num)>num)
-abnums=[num for num in range(28123+1) if d(num)>num]
-total=0
+
+isabnum = []
+for num in range(1, 28123 + 1):
+    isabnum.append(d(num) > num)
+abnums = [num for num in range(28123 + 1) if d(num) > num]
+total = 0
 for i in range(1, 28123 + 1):
-    ok=False
+    ok = False
     for abnum in abnums:
-        if abnum>=i:
+        if abnum >= i:
             break
-        if not isabnum[i-abnum-1]:
-            ok=False
+        if not isabnum[i - abnum - 1]:
+            ok = False
         else:
-            ok=True
+            ok = True
             break
-    if ok==False:
-        total+=i
+    if ok == False:
+        total += i
 print(total)
